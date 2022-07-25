be6a8a372493dc49aa0ebbe50a3f8ea52d4ba785
M-Irtaza <irtaza124000@gmail.com>
2022-07-25 10:48:38 +0500

    section3-complete

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,18 +4,19 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-
   recipes: Recipe[] = [
-    new Recipe('Pratha Roll', 'Description of Pratha roll', 
-  'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg'),
+    new Recipe(
+      'Pratha Roll',
+      'Description of Pratha roll',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg'
+    ),
+    
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
