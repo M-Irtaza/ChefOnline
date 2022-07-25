c08a076ac52bc85c01c06e9866dddb51e720e62d
M-Irtaza <irtaza124000@gmail.com>
2022-07-25 09:19:49 +0500

    makde recipe model

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Pratha Roll', 'Description of Pratha roll', 
+  'https://upload.wikimedia.org/wikipedia/commons/a/a8/Chicken-kathi-roll-recipe.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
